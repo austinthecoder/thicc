ea4431e897e1925c919f9f6fca367fd2e0c239a0
Austin Schneider <me@austinschneider.com>
2021-01-05 09:46:56 -0500

    bundle gem thicc

---
+++ lib/thicc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "thicc/version"
+
+module Thicc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/thicc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Thicc
+  VERSION = "0.1.0"
+end
